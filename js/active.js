3be4aac6a95f6895d7564503946c22061d0eb318
saeedane <dvdplay405@gmail.com>
2022-11-10 14:28:29 +0100

    update file css

--- js/active.js
+++ js/active.js
@@ -78,6 +78,18 @@ jQuery(window).scroll(function() {
     jQuery(this).parent(".menu_side").fadeOut();
   });
 
+ jQuery(".close_news").click(function () {
+            jQuery(this).parent(".newsConten").addClass("d-none"), localStorage.setItem("close_news", !0);
+        });
+        
+        if(localStorage.getItem("close_news") == 'true' ){
+           jQuery(".breakingBox").addClass("d-none");
+        }else{
+            jQuery(".breakingBox").removeClass("d-none");
+   
+        }
+  
+
 /*
   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 if (isMobile) {
