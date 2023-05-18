c5946a063d26519b527f626988d09c929712eed8
saeedane <dvdplay405@gmail.com>
2023-05-18 14:03:42 +0100

    update file css tadamsa

---
+++ js/sliders.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".infographicCont", {
+        slidesPerView: "auto",
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+            slidesPerView: 2,
+                breakpoints: { 320: { slidesPerView: 1, spaceBetween: 10 },   1200: { slidesPerView: 2},600:{slidesPerView: 2} },
+
+
+         navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
+
+var swiper = new Swiper(".categoryCont", {
+        slidesPerView: "auto",
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+            slidesPerView: 6,
+                breakpoints: { 320: { slidesPerView: 2, spaceBetween: 10 },   1200: { slidesPerView: 4},600:{slidesPerView: 2} },
+
+
+      });
+
